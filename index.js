94dcdc12e48e102b98e4696051afe19eefd69d14
AlbaevaE <46565391+AlbaevaE@users.noreply.github.com>
2024-02-13 14:12:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('questionnaireForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form submission logic here
+    alert('Form submitted!');
+});
